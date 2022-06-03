2175ab232a968317f2e9a9d40d8f872ad93a6b4e
asartorio <alejandro.sartorio@gmail.com>
2022-06-02 23:23:54 -0300

    cambiando el import de configuracion

---
+++ controllers/api_dolar_euro.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+def valor_dolar_euro():
+
+    imdb_url = 'https://www.bna.com.ar/Cotizador/MonedasHistorico'
+    imdb_response = requests.get(imdb_url)
+    imdb_soup = BeautifulSoup(imdb_response.text, 'html.parser')
+
+    lista = imdb_soup.find_all('td', {'class': 'dest'})
+    i = 0
+    dolar = ""
+    euro = ""
+
+    for ll in lista:
+
+        l = ll.text
+        i += 1
+
+        if i == 1:
+            dolar = l
+        if i == 5:
+            euro = l
+
+    return (dolar, euro)
--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from . import costo_almacenamiento
 from . import analitico
-from . import sale_order_cotizador
 from . import configuracion
+from . import sale_order_cotizador
